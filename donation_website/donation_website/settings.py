1105ea38d8ec6075560032f01ac99a230b8c9783
null_coder <104222189+nullfox-coder@users.noreply.github.com>
2024-05-10 20:26:25 +0530

    error -static files

--- donation_website/donation_website/settings.py
+++ donation_website/donation_website/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,7 +125,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "/static",
+    BASE_DIR / "static",
 ]
 
 EMAIL_HOST='smtp.gmail.com'
